eb5ec4317b74a88782847d7d1739b2f662c4ecf6
Sam Coy <samcoy3@gmail.com>
2023-12-09 23:00:18 +0000

    Initial pass at day 4 solutions

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -21,19 +21,48 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = game `sepBy1` endOfLine
+  where
+    game = do
+      "Card"
+      many1 space
+      decimal
+      ":"
+      many1 space
+      winningNumbers <- Set.fromList <$> decimal `sepBy1` many1 space
+      " |"
+      many1 space
+      numbersYouHave <- Set.fromList <$> decimal `sepBy1` many1 space
+      return (winningNumbers, numbersYouHave)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Set Int, Set Int)]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+cardScore :: Set Int -> Int
+cardScore winners =
+  if Set.null winners then 0 else 2 ^ (Set.size winners - 1)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . map (cardScore . uncurry Set.intersection)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB scratchcards = runScratchcards 1 (replicate (length scratchcards - 1) 0) scratchcards
+  where
+    runScratchcards numberOfCurrentCard futureCopyNumbers scratchcards =
+      let nextScratchcard = head scratchcards
+          nextScore = Set.size $ uncurry Set.intersection nextScratchcard
+          newFutureCopyNumbers = zipWith (+) futureCopyNumbers (replicate nextScore numberOfCurrentCard ++ repeat 0)
+       in if length scratchcards == 1
+            then numberOfCurrentCard
+            else
+              numberOfCurrentCard
+                + runScratchcards
+                  (head newFutureCopyNumbers + 1)
+                  (tail newFutureCopyNumbers)
+                  (tail scratchcards)
